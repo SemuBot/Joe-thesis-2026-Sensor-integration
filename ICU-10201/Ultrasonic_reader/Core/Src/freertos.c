3061be4e308f68ac55fe89b819304b78d8d4ef71
Aleks Jõe <aleks.joe@ut.ee>
2026-04-10 23:25:37 +0300

    Micro ros Successful build

--- ICU-10201/Ultrasonic_reader/Core/Src/freertos.c
+++ ICU-10201/Ultrasonic_reader/Core/Src/freertos.c
@@ -22,9 +22,13 @@
 #include "task.h"
 #include "main.h"
 #include "cmsis_os.h"
+#include <stdbool.h>
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
+
+
+#include "usart.h"
 
 /* USER CODE END Includes */
 
@@ -119,13 +123,65 @@ void MX_FREERTOS_Init(void) {
 /* USER CODE END Header_StartDefaultTask */
 void StartDefaultTask(void *argument)
 {
-  /* USER CODE BEGIN StartDefaultTask */
-  /* Infinite loop */
+  /* USER CODE BEGIN 5 */
+
+  // micro-ROS configuration
+
+  rmw_uros_set_custom_transport(
+    true,
+    (void *) &huart2,
+    cubemx_transport_open,
+    cubemx_transport_close,
+    cubemx_transport_write,
+    cubemx_transport_read);
+
+  rcl_allocator_t freeRTOS_allocator = rcutils_get_zero_initialized_allocator();
+  freeRTOS_allocator.allocate = microros_allocate;
+  freeRTOS_allocator.deallocate = microros_deallocate;
+  freeRTOS_allocator.reallocate = microros_reallocate;
+  freeRTOS_allocator.zero_allocate =  microros_zero_allocate;
+
+  if (!rcutils_set_default_allocator(&freeRTOS_allocator)) {
+      printf("Error on default allocators (line %d)\n", __LINE__);
+  }
+
+  // micro-ROS app
+
+  rcl_publisher_t publisher;
+  std_msgs__msg__Int32 msg;
+  rclc_support_t support;
+  rcl_allocator_t allocator;
+  rcl_node_t node;
+
+  allocator = rcl_get_default_allocator();
+
+  //create init_options
+  rclc_support_init(&support, 0, NULL, &allocator);
+
+  // create node
+  rclc_node_init_default(&node, "cubemx_node", "", &support);
+
+  // create publisher
+  rclc_publisher_init_default(
+    &publisher,
+    &node,
+    ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int32),
+    "cubemx_publisher");
+
+  msg.data = 0;
+
   for(;;)
   {
-    osDelay(1);
+    rcl_ret_t ret = rcl_publish(&publisher, &msg, NULL);
+    if (ret != RCL_RET_OK)
+    {
+      printf("Error publishing (line %d)\n", __LINE__);
+    }
+
+    msg.data++;
+    osDelay(10);
   }
-  /* USER CODE END StartDefaultTask */
+  /* USER CODE END 5 */
 }
 
 /* Private application code --------------------------------------------------*/
